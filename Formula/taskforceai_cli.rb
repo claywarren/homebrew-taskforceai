0c59d026bcc70acf768bd38e14b055dbf349e205
Clay Warren <36519094+ClayWarren@users.noreply.github.com>
2026-01-06 21:42:05 -0600

    Update URLs for taskforceai-cli downloads

--- Formula/taskforceai_cli.rb
+++ Formula/taskforceai_cli.rb
@@ -10,24 +10,17 @@ class TaskforceaiCli < Formula
 
   on_macos do
     on_arm do
-      url 'https://github.com/ClayWarren/grok4fastheavy/releases/download/v0.4.0/taskforceai-cli-arm64'
+      url 'https://github.com/ClayWarren/taskforceai/releases/download/v0.4.0/taskforceai-cli-arm64'
       sha256 'PLACEHOLDER_ARM64_SHA256' # This will be replaced by your CI/CD pipeline
     end
     on_intel do
-      url 'https://github.com/ClayWarren/grok4fastheavy/releases/download/v0.4.0/taskforceai-cli-amd64'
+      url 'https://github.com/ClayWarren/taskforceai/releases/download/v0.4.0/taskforceai-cli-amd64'
       sha256 'PLACEHOLDER_AMD64_SHA256' # This will be replaced by your CI/CD pipeline
     end
   end
 
   def install
-    # The correct binary is downloaded based on the architecture,
-    # but it retains its original name from the URL. We rename it to `taskforceai`
-    # so the user can simply run `taskforceai`.
-    if Hardware::CPU.arm?
-      bin.install 'taskforceai-cli-arm64' => 'taskforceai'
-    else
-      bin.install 'taskforceai-cli-amd64' => 'taskforceai'
-    end
+    bin.install "taskforceai"
   end
 
   test do
